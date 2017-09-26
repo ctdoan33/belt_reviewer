a6a1bcc593530709182b62f01c140ab1024b05bd
ctdoan33 <doanchristopher_1@yahoo.com>
2017-09-25 19:52:15 -0700

    initial commit

---
+++ apps/belt/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^books$', views.home),
+    url(r'^logout$', views.logout),
+    url(r'^books/add$', views.create),
+    url(r'^books/(?P<id>\d+)$', views.showbook),
+    url(r'^users/(?P<id>\d+)$', views.showuser),
+    url(r'^destroy/(?P<id>\d+)$', views.destroy),
+]
